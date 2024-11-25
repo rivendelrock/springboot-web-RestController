0607eb2f9c0916d145ad64d56a179920c1c57454
Rocío Marcos <rocio.marcos.abajo@accenture.com>
2024-11-25 17:50:56 +0100

    Ficherosproperties

--- src/main/java/com/rocio/curso/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/rocio/curso/springbootweb/controllers/PathVariableController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rocio.curso.springbootweb.models.DTO.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,5 +24,13 @@ public class PathVariableController {
         return param;
     }
     
+    @GetMapping("/mix/{product}/{id}")
+    public Map <String, Object> mixPathVar (@PathVariable String product, @PathVariable Long id){
+        
+        Map <String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        return json;
 
+    }
 }
